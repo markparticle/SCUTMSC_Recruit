c6f0ade020decbe25c7dfe989eab41d93ee92171
MARK-PC\MARK <mark.iii@live.com>
2019-01-18 15:10:54 +0800

    [FIX]更改创建数据库表命令

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ```
 $ pip install -r requirements.txt
-$ python manage.py makemigrations
+$ python manage.py makemigrations backend
 $ python manage.py migrate
 $ python manage.py runserver  
 ```
--- recruit/settings.py
+++ recruit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,6 +30,7 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+# sys.path.insert(0,os.path.join(BASE_DIR,'backend'))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
